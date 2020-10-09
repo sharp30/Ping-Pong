403ae32e43e28af5d09065be5a9462a3488aaff7
Ofir Shapira <ofirs2003@gmail.com>
2020-10-09 21:01:47 +0300

    Finished ball moving

--- Ball.py
+++ Ball.py
@@ -1,9 +1,13 @@
+import config
 class Ball:
     sign = '0'
-    def __init__(self):
+    def __init__(self,players):
         self.loc = [10,10]
         self.direction = [0 ]#random something
         self.speed = 1 #TODO
+        self.players = players      
+    
+    
     def __init__(self,start_loc,dir,speed):
         self.loc = start_loc
         self.direction = dir #random something
@@ -19,15 +23,27 @@ class Ball:
     def move(self):
         #check if some thing was hit
         #check if there was a goal
-        while not is_won(self):
+        while not (loc[0] == 0 or loc[1] == config.GAME_SIZE[1]): # while not making point
             if is_hit():
                 hit()
             else:
                 x =  self.loc[0] + self.speed
                 y =  self.direction[0] * x + self.direction[1]
             self.loc = [x,y]
-            #TODO:wait for a moment
+            #TODO:wait for a
 
+    #is ball in location
     def is_here(self,location):
         return Ball.sign if self.loc == location else None 
 
+    #is ball hitting something
+    def is_hit(self):
+        if self.players[0].is_here(loc) != None or self.players[1].is_here() != None:
+            return True
+
+        #TODO:check sides
+        for i in range(loc):
+            if loc[i] == 0 or loc[i] +1 == config.GAME_SIZE[i]:
+                return True
+        
+        return False
